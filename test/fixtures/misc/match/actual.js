d64bed66449f2017f37121c9bdc27efb58c9d959
William C. Johnson <wcjohnson@oigroup.net>
2017-05-26 16:16:00 -0400

    Fixtures for oigroup features

---
+++ test/fixtures/misc/match/actual.js
@@ -0,0 +1,9 @@
+match x:
+  | a: b
+  | c with (d) -> e
+  | f with {g}: h
+
+i = match x:
+  | a: b
+  | c with (d) -> e
+  | f with {g}: h
---
+++ test/fixtures/misc/match/expected.js
@@ -0,0 +1,15 @@
+"use strict";
+
+if (x === a) b;else if (x === c) (function (d) {
+  return e;
+})(x);else if (x === f) {
+  var _x = x,
+      g = _x.g;
+  h;
+}
+var i = x === a ? b : x === c ? function (d) {
+  return e;
+}(x) : x === f ? function (_ref) {
+  var g = _ref.g;
+  return h;
+}(x) : null;
