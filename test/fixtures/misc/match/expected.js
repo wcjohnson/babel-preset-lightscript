d7338558d43793d5590b2c0d7c86551f078847af
William C. Johnson <wcjohnson@oigroup.net>
2017-06-16 13:20:30 -0400

    Update fixtures for runtime import

--- test/fixtures/misc/match/expected.js
+++ test/fixtures/misc/match/expected.js
@@ -1,17 +1,21 @@
 "use strict";
 
-function _hasProps(obj) { if (obj == null) return false; if (typeof obj !== "object" && typeof obj !== "function") return false; for (var i = arguments.length - 1; i > 0; i--) { if (!(arguments[i] in obj)) return false; } return true; }
+var _hasProps2 = require("@oigroup/lightscript-runtime/hasProps");
+
+var _hasProps3 = _interopRequireDefault(_hasProps2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 if (a) {
   b;
-} else if (f && _hasProps(x, "g")) {
+} else if (f && (0, _hasProps3.default)(x, "g")) {
   var _x = x,
       _g = _x.g;
   h;
 }var i = function (it) {
   if (a) {
     return b;
-  } else if (f && _hasProps(it, "g")) {
+  } else if (f && (0, _hasProps3.default)(it, "g")) {
     var _g2 = it.g;
     return h;
   }
