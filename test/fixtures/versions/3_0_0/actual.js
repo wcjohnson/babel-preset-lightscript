7ef098791b9392136de59a3e51095b6d68899578
William C. Johnson <wcjohnson@oigroup.net>
2017-10-03 16:40:34 -0400

    Fast-forward self host to 3.0
    commit e493ff968f8b83de8a93000d1118ad78c1263fde
    Author: William C. Johnson <wcjohnson@oigroup.net>
    Date:   Tue Oct 3 15:26:04 2017 -0400
    
        @oigroup/babel-preset-lightscript@3.0.0-alpha.4
    
    commit a0a5c1e2982464a119534df4530272def685f4eb
    Author: William C. Johnson <wcjohnson@oigroup.net>
    Date:   Mon Oct 2 21:36:19 2017 -0400
    
        @oigroup/babel-preset-lightscript@3.0.0-alpha.3
    
    commit c7b4ae66e511332b6940cf13175cbc499b1db7bb
    Author: William C. Johnson <wcjohnson@oigroup.net>
    Date:   Mon Oct 2 21:29:10 2017 -0400
    
        Update 3.0 fixture
    
    commit 8ef63ccd382c199c24873e63f9fa7ff519ba5819
    Author: William C. Johnson <wcjohnson@oigroup.net>
    Date:   Sat Sep 30 22:09:46 2017 -0400
    
        @oigroup/babel-preset-lightscript@3.0.0-alpha.2
    
    commit ebd57f43b946baf0683843f8ddca74fab75e6232
    Author: William C. Johnson <wcjohnson@oigroup.net>
    Date:   Thu Sep 28 21:27:29 2017 -0400
    
        @oigroup/babel-preset-lightscript@3.0.0-alpha.1
    
    commit 9410221e8e9081bde92e09d55099b15e5fb68f0f
    Author: William C. Johnson <wcjohnson@oigroup.net>
    Date:   Thu Sep 28 21:25:42 2017 -0400
    
        Additional 3.0 tests
    
    commit b1b8f796c31ebf173d7ac181e4112db111abc42b
    Author: William C. Johnson <wcjohnson@oigroup.net>
    Date:   Wed Sep 27 22:17:25 2017 -0400
    
        @oigroup/babel-preset-lightscript@3.0.0-alpha.0
    
    commit fce665b1c2c0b426866c6155f9b77dcaadf3d075
    Author: William C. Johnson <wcjohnson@oigroup.net>
    Date:   Wed Sep 27 22:09:24 2017 -0400
    
        3.0 tests
    
    commit 6d1782064661ba156ed95d9971f0efe177a41a14
    Author: William C. Johnson <wcjohnson@oigroup.net>
    Date:   Mon Sep 25 16:01:28 2017 -0400
    
        @oigroup/babel-preset-lightscript@2.3.1
    
    commit 92a8013901d7e97bbb822f54573ac3509fb9c916
    Author: William C. Johnson <wcjohnson@oigroup.net>
    Date:   Mon Sep 25 16:00:22 2017 -0400
    
        Fix dep
    
    commit ac6c7debe3a5a0756f699bf0118e9a1ec2db2ed6
    Author: William C. Johnson <wcjohnson@oigroup.net>
    Date:   Mon Sep 25 15:54:43 2017 -0400
    
        @oigroup/babel-preset-lightscript@2.3.0
    
    commit 2d8eae53fb2f80926ee7a4d7958b8773294dfb52
    Author: William C. Johnson <wcjohnson@oigroup.net>
    Date:   Sat Jul 15 23:18:34 2017 -0400
    
        @oigroup/babel-preset-lightscript@2.2.1
    
    commit 217feac3d2e11dd909b5e376e4c5c6551c079942
    Author: William C. Johnson <wcjohnson@oigroup.net>
    Date:   Fri Jul 14 23:50:35 2017 -0400
    
        @oigroup/babel-preset-lightscript@2.2.0
    
    commit c203862242e33ab2c3cc7b7152a796254448f271
    Author: William C. Johnson <wcjohnson@oigroup.net>
    Date:   Thu Jul 13 18:21:09 2017 -0400
    
        @oigroup/babel-preset-lightscript@2.1.1
    
    commit ab31dccc8abd5d8703ab10d8cd189411c3349bcd
    Author: William C. Johnson <wcjohnson@oigroup.net>
    Date:   Thu Jul 13 18:18:03 2017 -0400
    
        Move `transform-decorators-legacy` before `transform-class-properties`
    
        Fixes lightscript/lightscript#41
    
    commit fb87b54ac6913f648416ecc3029885c02a456e78
    Author: William C. Johnson <wcjohnson@oigroup.net>
    Date:   Thu Jul 6 20:48:06 2017 -0400
    
        @oigroup/babel-preset-lightscript@2.1.0
    
    commit d2d062d86fb8f2c6e3e7bda9e3423592c96b8eaf
    Author: William C. Johnson <wcjohnson@oigroup.net>
    Date:   Thu Jul 6 20:46:51 2017 -0400
    
        Add config directive test
    
    commit 8b8335385e133e0e295a52e8f228627d58c9668b
    Author: William C. Johnson <wcjohnson@oigroup.net>
    Date:   Mon Jun 26 14:40:34 2017 -0400
    
        Update deps
    
    commit 55084056b2c47c713747114ed56f7b1b3f0768ca
    Author: William C. Johnson <wcjohnson@oigroup.net>
    Date:   Mon Jun 26 14:24:04 2017 -0400
    
        @oigroup/babel-preset-lightscript@2.0.1
    
    commit 9ae4056016619dc1b659d25528bc45be11a18202
    Author: William C. Johnson <wcjohnson@oigroup.net>
    Date:   Mon Jun 26 14:22:16 2017 -0400
    
        Add preversion testing

---
+++ test/fixtures/versions/3_0_0/actual.js
@@ -0,0 +1,3 @@
+a!
+
+[...for elem e in arr: e]
---
+++ test/fixtures/versions/3_0_0/expected.js
@@ -0,0 +1,9 @@
+"use strict";
+
+a();
+
+(function () {
+  var _arr = [];for (var _i2 = 0, _len2 = arr.length; _i2 < _len2; _i2++) {
+    var _e = arr[_i2];_arr.push(_e);
+  }return _arr;
+})();
